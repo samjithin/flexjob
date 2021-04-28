2ef60b2ce99de287fc650ca1a5ae669dbdb3a876
samjithin <samjithin@gmail.com>
2021-04-28 16:53:14 +0530

    Gcloud

--- gcloud.tf
+++ gcloud.tf
@@ -7,7 +7,7 @@ module "gcloud" {
   skip_download = "false"
 
   create_cmd_entrypoint  = "${path.cwd}/scripts/script.sh"
-  create_cmd_body        = "enable terraformlearning-284414"
-  destroy_cmd_entrypoint = "gcloud"
-  destroy_cmd_body       = "version"
+  #create_cmd_body        = "enable terraformlearning-284414"
+  #destroy_cmd_entrypoint = "gcloud"
+  #destroy_cmd_body       = "version"
 }
---
+++ scripts/script.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+GCLOUD_LOCATION=$(command -v gcloud)
+echo "Using gcloud from $GCLOUD_LOCATION"
+
+gcloud --version
+echo "running gcloud services $1 appengine.googleapis.com --project $2"
+gcloud services enable appengine.googleapis.com --project terraformlearning-284414
