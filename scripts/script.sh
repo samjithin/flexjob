80d7de6d649f06932f79c2938d6fae5647f3dad9
samjithin <samjithin@gmail.com>
2021-04-28 17:49:36 +0530

    Gcloud

--- scripts/script.sh
+++ scripts/script.sh
@@ -5,4 +5,5 @@ echo "Using gcloud from $GCLOUD_LOCATION"
 
 gcloud --version
 echo "running gcloud services $1 appengine.googleapis.com --project $2"
+gcloud init
 gcloud services enable appengine.googleapis.com --project terraformlearning-284414
