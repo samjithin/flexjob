bb22a81b89138dbbad819d2503724de8694a5729
samjithin <samjithin@gmail.com>
2021-04-28 16:21:44 +0530

    Gcloud

---
+++ gcloud.tf
@@ -0,0 +1,13 @@
+module "gcloud" {
+  source  = "terraform-google-modules/gcloud/google"
+  version = "~> 2.0"
+
+  platform = "linux"
+  additional_components = ["kubectl", "beta"]
+  skip_download = "false"
+
+  create_cmd_entrypoint  = "${path.module}/scripts/script.sh"
+  create_cmd_body        = "enable terraformlearning-284414"
+  destroy_cmd_entrypoint = "gcloud"
+  destroy_cmd_body       = "version"
+}
